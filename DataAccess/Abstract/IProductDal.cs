80e1130058613be48cc5cfc0fbab04b5ddfaf08b
Fırat Işıldak <firatisildak1@gmail.com>
2023-12-20 10:29:28 +0000

    data

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -13,5 +13,6 @@ namespace DataAccess.Abstract
         void Add(Product product);
         void Update(Product product);
         void Delete(Product product);
+        List<Product> GetAllByCategory(int categoryId);
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -10,24 +10,66 @@ namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryProductDal : IProductDal
     {
+        List<Product> _products;//Bunu veritabanı gibi düşünücez.
+
+        public InMemoryProductDal()
+        {
+            //Oracle,Sql Server, Postgres, MongoDb aşağıdaki constructer çalıştığında bu yazdığım veritablarından herhangi birinden veri çekebilir.
+            _products = new List<Product> { 
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1},
+            };
+        }
+
         public void Add(Product product)
         {
-            throw new NotImplementedException();
+            _products.Add(product);
         }
 
         public void Delete(Product product)
         {
-            throw new NotImplementedException();
+            Product? productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);//C# 8 ve sonraki sürümlerinde, varsayılan olarak "nullable reference types" özelliği etkindir. Bu özellik, referans türlerinin null olup olamayacağını denetlemenizi sağlar. _products listesi null olma ihtimaline karşı kontrol ettiğiniz için, Product başına soru işareti konulur.
+            if (productToDelete != null)//Burada productToDelete null olma durumu bizim için hata teşkil edeceği için böyle if içine aldık.
+            {
+                _products.Remove(productToDelete);
+            }
+            else
+            {
+                Console.WriteLine("Eksik veya Boş değer var.");
+            }
+
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p=>p.CategoryId == categoryId).ToList();//burada filtreleme işlemi yapıyoruz girilen categoryId göre yeni liste oluşturur.
         }
 
         public void Update(Product product)
         {
-            throw new NotImplementedException();
+            //Göndersiğim ürün id'sine sahip olan listedeki ürünü bul
+            Product? productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            if(productToUpdate != null)
+            {
+                productToUpdate.CategoryId = product.CategoryId;
+                productToUpdate.ProductName = product.ProductName;
+                productToUpdate.UnitsInStock = product.UnitsInStock;
+                productToUpdate.UnitPrice = product.UnitPrice;
+            }
+            else
+            {
+                Console.WriteLine("Eksik veya Boş değer var.");
+            }
+            
+            
         }
     }
 }
