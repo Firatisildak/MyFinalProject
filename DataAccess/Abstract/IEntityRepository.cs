392a5ab6a2c1ac1f0ebfb48a0ac9a935b32ac0c2
Fırat Işıldak <firatisildak1@gmail.com>
2024-01-01 16:27:06 +0000

    generic constraint

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Entities.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //generic constraint:genel kısıtlama bunu whereden sonraki kısım için yazdık
+    //class:referans tip
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new(): new'lenebilir olmalı. Bu sayede IEntity yani interface kullanımını sınırladık.
+    public interface IEntityRepository<T> where T : class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>>? filter = null);
         T Get(Expression<Func<T, bool>> filter);
