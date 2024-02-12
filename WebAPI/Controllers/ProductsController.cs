db10891ef1da4d99d4bebdbe9b408597c7044739
Fırat Işıldak <firatisildak1@gmail.com>
2024-02-12 17:55:17 +0000

    I wrote my own controller

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled: gevşek bağımlılık
+        //naming convention
+        //IoC Container -- Inversion of Control
+        IProductServise _productsService;
+
+        public ProductsController(IProductServise productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result=_productsService.GetAll();
+            return result.Data;
+
+        }
+
+    }
+}
