a0bea48ae5b38e48bfd1892730b41f04421a338f
Fırat Işıldak <firatisildak1@gmail.com>
2024-02-14 11:25:04 +0000

    get and post request fonctions written

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -19,12 +19,40 @@ namespace WebAPI.Controllers
             _productsService = productsService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
+            //Swagger
+            //Dependency chain --
             var result=_productsService.GetAll();
-            return result.Data;
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
 
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result=_productsService.GetById(id);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productsService.Add(product);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
 
     }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,8 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,10 +11,14 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IProductServise, ProductManager>();
+builder.Services.AddSingleton<IProductDal, EfProductDal>();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
+app.UseSwagger();
 app.UseAuthorization();
 
 app.MapControllers();
