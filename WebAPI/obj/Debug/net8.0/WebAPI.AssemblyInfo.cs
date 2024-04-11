3de1e5fc23301b5afbfa5c2644944d3ddf0c5bf0
Fırat Işıldak <firatisildak1@gmail.com>
2024-04-11 09:49:24 +0100

    last commit

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1829fa4df2835875503f4a1b738e5f36323f915a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb3d6e2267fdf448190543d448a7dbc709590aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
